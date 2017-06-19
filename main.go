8fc704da4e2b27989f2069c372b85a0db3bff6e9
xueye <281431280@qq.com>
2017-06-19 16:29:41 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+//281431280@qq.com
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Config struct {
+	Port int
+}
+
+func main() {
+	config := Config{}
+	bytes, err := ioutil.ReadFile("./config.json")
+	err = json.Unmarshal(bytes, &config)
+	wd, err := os.Getwd()
+	http.Handle("/", http.FileServer(http.Dir(wd)))
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("server start on:%d", config.Port)
+	http.ListenAndServe(fmt.Sprintf(":%d", config.Port), nil)
+}
